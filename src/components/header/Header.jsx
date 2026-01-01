e50c6ac1649f781454dd9dc4053f49c8f7e131e2
abrar997 <abraralrawi997.com>
2026-01-01 22:29:12 +0300

    user data in header completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,56 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { Links } from "../../assets/data";
 import { FaShoppingCart } from "react-icons/fa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { BsX } from "react-icons/bs";
 import { CgProfile, CgUser, CgUserAdd } from "react-icons/cg";
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../firebase/config";
 import { toast, ToastContainer } from "react-toastify";
 import { BiLogOut } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { SET_ACTIVE_USER } from "../../redux/slice/authSlice";
 
 const Header = () => {
   const [show, setShow] = useState(false);
   const [mainMenu, setMainMenu] = useState(false);
+  const [displayName, setDisplayName] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   // active link style
   const active = "text-teal-600 text-lg border-b capitalize px-1";
   const activeLink = ({ isActive }) =>
     isActive ? `${active}` : "text-lg capitalize";
-  const navigate = useNavigate();
 
-  // current user
-  // const user = auth.currentUser;
-  // const nameOfUser = user.email;
+  // monitor currently signed in user
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // console.log(user);
+        // const uid = user.uid;
+        if (user.displayName === null) {
+          setDisplayName(user.email.slice(0, 10));
+        } else {
+          setDisplayName(user.displayName.slice(0, 6));
+        }
+        setPhotoURL(user.photoURL);
+        dispatch(
+          SET_ACTIVE_USER({
+            email: user.email,
+            userName: user.displayName,
+            userID: user.uid,
+            photoURL: user.photoURL,
+          })
+        );
+      } else {
+        setDisplayName("");
+        setPhotoURL("");
+      }
+    });
+  }, []);
 
   const toggleMenu = () => {
     setShow(!show);
@@ -51,7 +80,8 @@ const Header = () => {
           mainMenu={mainMenu}
           setMainMenu={setMainMenu}
           UserLogout={UserLogout}
-          // nameOfUser={nameOfUser}
+          nameOfUser={displayName}
+          photoURL={photoURL}
         />
         <MobileNav
           links={Links}
@@ -74,10 +104,11 @@ const DesktopNav = ({
   setMainMenu,
   UserLogout,
   nameOfUser,
+  photoURL,
 }) => {
   return (
     <div className="lg:grid grid-cols-4 w-full shadow-lg p-3 px-3 hidden bg-[#403F3F] mt-4 items-center">
-      <div className="col-span-1 text-xl">a-Shop</div>
+      <div className="col-span-1 text-2xl">A-Shop</div>
       <div className="flex gap-5 items-start col-span-2 m-auto">
         {links.map((link, idx) => (
           <NavLink to={link.to} key={idx} className={isActive}>
@@ -86,7 +117,7 @@ const DesktopNav = ({
         ))}
       </div>
 
-      <div className="flex items-center gap-4 col-span-1 ml-auto">
+      <div className="flex items-center gap-5 col-span-1 ml-auto">
         <Link
           to="/cart"
           className="text-xl border border-teal-600 rounded p-2 hover:text-teal-600"
@@ -100,13 +131,23 @@ const DesktopNav = ({
           Login
         </Link>
         <div>
-          {nameOfUser && <p>{nameOfUser}</p>}
-          <button
-            onClick={() => setMainMenu(!mainMenu)}
-            className="bg-slate-50 rounded-full w-9 h-9 text-teal-600 flex items-center justify-center text-2xl"
-          >
-            <CgUser />
-          </button>
+          <div className="flex items-center gap-2">
+            <button
+              onClick={() => setMainMenu(!mainMenu)}
+              className="bg-slate-50 rounded-full w-9 h-9 text-teal-600 flex items-center justify-center text-2xl"
+            >
+              {photoURL ? (
+                <img src={photoURL} alt="user image" className="rounded-full" />
+              ) : (
+                <CgUser />
+              )}
+            </button>
+            {nameOfUser && (
+              <p className="text-sm text-teal-500 font-semibold">
+                {nameOfUser}
+              </p>
+            )}
+          </div>
           {mainMenu && (
             <div className="w-38 rounded bg-slate-50 text-black border grid absolute right-10 top-20 z-50">
               <Link
@@ -124,7 +165,7 @@ const DesktopNav = ({
               >
                 <span className="text-lg text-teal-600 border rounded p-1">
                   <BiLogOut />
-                </span>{" "}
+                </span>
                 <span>Logout</span>
               </button>
               <Link
@@ -133,7 +174,7 @@ const DesktopNav = ({
               >
                 <span className="text-lg text-teal-600 border rounded p-1">
                   <CgProfile />
-                </span>{" "}
+                </span>
                 <span>Profile</span>
               </Link>
             </div>
